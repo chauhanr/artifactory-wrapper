e871631f56db7fa71b6a978c3b750467a10255f3
Ritesh Chauhan <chauhanx.ritesh@gmail.com>
2020-01-09 05:37:19 +0000

    filebased

--- main.go
+++ main.go
@@ -33,26 +33,21 @@ func Upload(w http.ResponseWriter, r *http.Request) {
 		}
 		defer file.Close()
 		log.Printf("Upload file: %+v size: %+v header: %+v\n", handler.Filename, handler.Size, handler.Header)
-		tmp, err := ioutil.TempFile("", "ios-sdk.framework")
-		if err != nil {
-			log.Printf("Error in temp file creation %s\n", err)
-		}
-		defer tmp.Close()
+
 		fb, err := ioutil.ReadAll(file)
 		if err != nil {
 			http.Error(w, "error reading from uploaded file", http.StatusInternalServerError)
 			return
 		}
-		tmp.Write(fb)
-		in, err := tmp.Stat()
+
+		err = ioutil.WriteFile(handler.Filename, fb, 0644)
 		if err != nil {
 			log.Printf("Error: %s", err)
 			http.Error(w, "error writing file", http.StatusInternalServerError)
 			return
 		}
-		log.Printf("Size of the new file: %d\n", in.Size)
 		/* Now we need to send the temp file to the Artifactory Client*/
-		err = PublishToArtifactory(tmp, "mvn-local", "HelloWorldSDK", "0.0.1", handler.Filename)
+		err = PublishToArtifactory(handler.Filename, "mvn-local", "HelloWorldSDK", "0.0.1", handler.Filename)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		} else {
@@ -66,21 +61,25 @@ func Upload(w http.ResponseWriter, r *http.Request) {
 
 var BASE_URL = "http://localhost:8080/artifactory"
 
-func PublishToArtifactory(data *os.File, repo string, framework string, version string, fname string) error {
+func PublishToArtifactory(dataFile string, repo string, framework string, version string, fname string) error {
+	data, err := os.Open(dataFile)
+	if err != nil {
+		return err
+	}
 	url := prepareArtifactoryUploadURL(repo, framework, version, fname)
 	r, err := http.NewRequest("PUT", url, data)
 	if err != nil {
 		log.Printf("Error preparing the new request %s\n", err)
 		return err
 	}
-	r.SetBasicAuth("admin", "password") 
+	r.SetBasicAuth("admin", "password")
 	client := &http.Client{}
 	res, err := client.Do(r)
 	if err != nil {
 		log.Printf("Error publishing the file to artifactory %s\n", err)
 	}
 	code := res.StatusCode
-	
+
 	bs, _ := ioutil.ReadAll(res.Body)
 	fmt.Printf("Artifactory Status Code %d, Response Body: %s\n", code, string(bs))
 
@@ -88,7 +87,7 @@ func PublishToArtifactory(data *os.File, repo string, framework string, version 
 		log.Printf("Error uploading file to artifactory %d\n", code)
 		return errors.New("Unable to upload file successfully to artifactory")
 	}
-	
+
 	defer res.Body.Close()
 	return nil
 }
